a9869994b3e1e1236ce3baf8aac98e355e8f21f3
zhigalov <zhigalov@ya.ru>
2024-12-20 12:58:21 +0300

    start

---
+++ app.py
@@ -0,0 +1,132 @@
+from datetime import datetime
+from flask import Flask, json, jsonify, render_template, request, redirect, url_for, session, flash
+from flask_socketio import SocketIO, emit, join_room, leave_room
+import db
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+socketio = SocketIO(app)
+
+@app.route('/') # Индекс
+def start_page():
+    print(session)
+    db.create_tables()
+    return render_template('index.html')
+
+@app.route('/choose_role') # выбор роли (стилист\клиент)
+def choose_role():
+    return render_template('choose_role.html')
+
+@app.route('/auth') # авторизация
+def auth_page():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        userInfo = db.get_user_info_by_email(email)
+
+        # Исправление: проверка на наличие userInfo и его структуры
+        if userInfo and email == userInfo.get('email'):
+            print('Правильное мыло')
+            if password == userInfo.get('password'):
+                print('Правильный пароль')
+                session['email'] = email
+                return redirect(url_for('start_page'))  # При удачной авторизации перенаправляется на страницу index
+            else:
+                #flash(message='Неправильный пароль')
+                print('Неправильный пароль')
+        else:
+            #flash(message='Такого пользователя не существует')
+            print('Такого пользователя не существует')
+    return render_template('auth.html')
+
+@app.route('/registrationCL', methods=['GET', 'POST'])  # регистрация клиента
+def registrationCL_page():
+    if request.method == 'POST':
+        #данные регистрации
+        email = request.form.get('email')
+        first_name = request.form.get('name')
+        last_name = request.form.get('surname')
+        password = request.form.get('password')
+        birth_date = request.form.get('dob')
+        isStylist = False
+        level = 0
+
+        #параметры пользователя
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        chest_size = request.form.get('chest')
+        ass_size = request.form.get('hips')
+        waist_size = request.form.get('waist') # талия
+        clothes_size = request.form.get('size')
+
+        print(f'{email}, {first_name}, height: {height}')
+
+        #retypedPassword = request.form.get('retypedPassword')
+        userInfo = db.get_user_info_by_email(email)
+
+        if userInfo is None:
+            #if password == retypedPassword:
+
+                db.add_user(first_name= first_name, last_name= last_name, email= email, password= password,
+                             birth_date= birth_date, level= level, photo_path= '', stylist= isStylist)
+                db.add_user_params(height, weight, chest_size, ass_size, waist_size, clothes_size, email)
+
+                print('Регистрация успешна!')
+                
+                return redirect(url_for('anket_gender'))  # Редирект на страницу выбра пола анкета
+           #else:
+                #flash(message='Пароли не совпадают')
+        else:
+            #flash(message='Эта почта уже используется')
+            print('Эта почта уже используется')
+
+    return render_template('registration.html')  # Возвращаем форму регистрации для GET-запроса
+
+@app.route('/registrationST', methods=['GET', 'POST'])  # регистрация стилиста
+def registrationST_page():
+    if request.method == 'POST':
+        #данные регистрации
+        email = request.form.get('email')
+        first_name = request.form.get('name')
+        last_name = request.form.get('surname')
+        password = request.form.get('password')
+        #birth_date = request.form.get('birth_date')
+        isStylist = True
+        level = 1
+
+        #Тут надо сделать загрузку файлов## на хуй посланы
+
+        #retypedPassword = request.form.get('retypedPassword')
+        userInfo = db.get_user_info_by_email(email)
+
+        if userInfo is None:
+            #if password == retypedPassword:
+                db.add_user(first_name= first_name, last_name= last_name, email= email, 
+                             birth_date='NULL', password= password, level= level, photo_path= '', stylist= isStylist)
+                flash('Регистрация успешна!')
+                return redirect(url_for('start_page'))  # Редирект на страницу входа после регистрации ???
+           #else:
+                flash(message='Пароли не совпадают')
+        else:
+            print('Эта почта уже используется')
+
+    return render_template('registrationStilist.html')  # Возвращаем форму регистрации для GET-запроса
+
+@app.route('/anket-gender', methods = ['POST', 'GET']) # выбор пола для анкеты
+def anket_gender():
+    return render_template('gender.html') # after that target woman
+
+@app.route('/anket-purpose', methods = ['POST', 'GET']) # первая страниц анкеты(цели)
+def anket_purpose():
+    if request.method == 'POST':
+        print(request.args.get('home1'))
+    return render_template('targetWoman.html')
+
+@app.route('/users')
+def users():
+    return db.get_users()
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ db.py
@@ -0,0 +1,145 @@
+import sqlite3
+from datetime import datetime
+
+def get_db_connection():
+    conn = sqlite3.connect('project.db')
+    conn.row_factory = sqlite3.Row  # Для обращения к данным по имени столбца
+    return conn
+
+# Создание таблиц
+def create_tables():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        birth_date TEXT,
+        stylist BOOLEAN DEFAULT 0,
+        level INTEGER DEFAULT 0,
+        photo_path TEXT
+    )
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_parameters (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        height REAL,
+        weight REAL,
+        chest_size REAL,
+        ass_size REAL,
+        waist_size REAL,
+        clothes_size TEXT,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+    )
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS stylist_docs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        resume_path TEXT,
+        diploma_path TEXT,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+    )
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_chats (
+        user_chats_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        chat_id INTEGER,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS messages (
+        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        chat_id INTEGER NOT NULL,
+        creator_id INTEGER NOT NULL,
+        message TEXT NOT NULL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (chat_id) REFERENCES user_chats(chat_id),
+        FOREIGN KEY (creator_id) REFERENCES users(user_id)
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS chat_read_status (
+        chat_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        last_read TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (chat_id, user_id),
+        FOREIGN KEY (chat_id) REFERENCES user_chats(chat_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+    ''')
+
+    conn.commit()
+    conn.close()
+
+# добавление пользователя
+def add_user(first_name, last_name, password, email, birth_date, stylist, level, photo_path): # Добавить исключение и возвращение результата
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    INSERT INTO users (first_name, last_name, password, email, birth_date, stylist, level, photo_path)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+    ''', (first_name, last_name, password, email, birth_date, stylist, level, photo_path))
+    conn.commit()
+    conn.close()
+
+# добавление параметров пользователя
+def add_user_params(height, weight, chest_size, ass_size, waist_size, clothes_size, email):
+    user_id=get_user_info_by_email(email)['user_id']
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    INSERT INTO user_parameters (user_id, height, weight, chest_size, ass_size, waist_size, clothes_size)
+    VALUES(?, ?, ?, ?, ?, ?, ?) 
+    ''',(user_id, height, weight, chest_size, ass_size, waist_size, clothes_size))
+
+    conn.commit()
+    conn.close()
+
+# получение информации о пользователе по email
+def get_user_info_by_email(email):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+    user_info = cursor.fetchone()
+    conn.close()
+    return dict(user_info) if user_info else None
+
+# получение информации о пользователе по id
+def get_user_info_by_id(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+    user_info = cursor.fetchone()
+    conn.close()
+    return dict(user_info) if user_info else None
+
+
+
+
+
+def get_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users JOIN user_parameters ON users.user_id = user_parameters.user_id')
+    rows = cursor.fetchall()
+    conn.close()
+    return [dict(row) for row in rows]
